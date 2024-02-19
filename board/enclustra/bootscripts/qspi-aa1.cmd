6faa516a3e71879b00ca28233e7f7dcf30f54a2f
Andreas Buerkler <andreas.buerkler@enclustra.com>
2024-02-19 16:08:37 +0000

    Add dtbo for AA1 boot

--- board/enclustra/bootscripts/qspi-aa1.cmd
+++ board/enclustra/bootscripts/qspi-aa1.cmd
@@ -3,4 +3,5 @@ sf probe
 sf read ${ram_addr_kernel} ${qspi_offset_addr_kernel} ${size_kernel}
 sf read ${ram_addr_devicetree} ${qspi_offset_addr_devicetree} ${size_devicetree}
 sf read ${ram_addr_rootfs} ${qspi_offset_addr_rootfs} ${size_rootfs}
+run fdtload_qspi
 bootm ${ram_addr_kernel} ${ram_addr_rootfs} ${ram_addr_devicetree}
